52470eaf954f0139437004f92b5e1f848bbe391a
actionanand <meetanandns@gmail.com>
2020-05-29 21:25:11 +0530

    webpack with babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Webpack works...')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>Hello world!</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
+
+// const template = React.createElement('p', {}, 'testing 123');
+// ReactDOM.render(template, document.getElementById('app'));
+
