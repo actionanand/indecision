778fceda6fb21f487a9d6588da1cf9997d16ac1b
actionanand <meetanandns@gmail.com>
2020-05-31 00:33:43 +0530

    final touch up

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
     },
-  {
-    test: /\.s?css$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      'sass-loader'
-    ]
-  }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+    {
+      test: /\.s?css$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }]
+    },
+    plugins: [
+      new MiniCssExtractPlugin()
+    ],
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  };
 };
